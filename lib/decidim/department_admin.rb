96a8264b63094e160f4591fbfead18841f1a25fe
Oliver Valls <oliver.vh@coditramuntana.com>
2019-04-03 13:27:22 +0200

    [REFACTOR] Apply rubocop autocorrections.

--- lib/decidim/department_admin.rb
+++ lib/decidim/department_admin.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require "decidim/department_admin/admin"
-require "decidim/department_admin/engine"
-require "decidim/department_admin/admin_engine"
-require "decidim/department_admin/component"
+require 'decidim/department_admin/admin'
+require 'decidim/department_admin/engine'
+require 'decidim/department_admin/admin_engine'
+require 'decidim/department_admin/component'
 
 module Decidim
   # This namespace holds the logic of the `DepartmentAdmin` component. This component
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-require "decidim/dev"
+require 'decidim/dev'
 
-ENV["ENGINE_ROOT"] = File.dirname(__dir__)
+ENV['ENGINE_ROOT'] = File.dirname(__dir__)
 
 # require "decidim/admin"
 # require "decidim/core"
 # require "decidim/core/test"
 
-Decidim::Dev.dummy_app_path = File.expand_path(File.join("spec", "decidim_dummy_app"))
+Decidim::Dev.dummy_app_path = File.expand_path(File.join('spec', 'decidim_dummy_app'))
 
-require "decidim/dev/test/base_spec_helper"
+require 'decidim/dev/test/base_spec_helper'
 
 require 'decidim/department_admin'
