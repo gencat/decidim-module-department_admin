a4f01ac361e36ed62a9a5b8b3843d8fd4d52a8f0
Oliver Valls <oliver.vh@coditramuntana.com>
2019-05-06 11:57:55 +0200

    [REFACTOR] Remove commented lines.

--- lib/decidim/department_admin/test/factories.rb
+++ lib/decidim/department_admin/test/factories.rb
@@ -3,14 +3,6 @@
 require 'decidim/core/test/factories'
 
 FactoryBot.define do
-  # factory :department_admin_component, parent: :component do
-  #   name { Decidim::Components::Namer.new(participatory_space.organization.available_locales, :department_admin).i18n_name }
-  #   manifest_name { :department_admin }
-  #   participatory_space { create(:participatory_process, :with_steps) }
-  # end
-
-  # Add engine factories here
-
   factory :department_admin, parent: :user do
     roles { ['department_admin'] }
     transient do
