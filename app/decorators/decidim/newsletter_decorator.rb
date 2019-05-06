972cddb134e8f1dd37482cbfae55cc1c4eb89616
Isaac Massot <isaac.mg@coditramuntana.com>
2019-05-06 10:31:43 +0200

    fix comment method name

--- app/decorators/decidim/newsletter_decorator.rb
+++ app/decorators/decidim/newsletter_decorator.rb
@@ -5,7 +5,7 @@ require_dependency 'decidim/newsletter'
 Decidim::Newsletter.class_eval do
   # The area of the newsletter is the same
   # than the area of the author of the newsletter.
-  # The method name is `author` because the permissions system
+  # The method name is `area` because the permissions system
   # must verify that the `user` has the same `area` as the `resource`.
   def area
     return unless author.areas.any?
