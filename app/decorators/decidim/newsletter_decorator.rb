77d61d2bc7b56ff0208454b2b4b207e9877a0af0
Isaac Massot <isaac.mg@coditramuntana.com>
2019-05-06 08:45:04 +0200

    add missing comments

--- app/decorators/decidim/newsletter_decorator.rb
+++ app/decorators/decidim/newsletter_decorator.rb
@@ -5,6 +5,8 @@ require_dependency 'decidim/newsletter'
 Decidim::Newsletter.class_eval do
   # The area of the newsletter is the same
   # than the area of the author of the newsletter.
+  # The method name is `author` because the permissions system
+  # must verify that the `user` has the same `area` as the `resource`.
   def area
     return unless author.areas.any?
     return author.areas.first if author&.department_admin?
