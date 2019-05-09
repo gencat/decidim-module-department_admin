80c212d2c0443a9f604ec4373758a39d37524a5d
Oliver Valls <oliver.vh@coditramuntana.com>
2019-05-09 16:05:21 +0200

    Remove component declaration for department_admin.

--- lib/decidim/department_admin.rb
+++ lib/decidim/department_admin.rb
@@ -3,10 +3,9 @@
 require 'decidim/department_admin/admin'
 require 'decidim/department_admin/engine'
 require 'decidim/department_admin/admin_engine'
-require 'decidim/department_admin/component'
 
 module Decidim
-  # This namespace holds the logic of the `DepartmentAdmin` component. This component
+  # This namespace holds the logic of the `DepartmentAdmin` module. This module
   # allows users to create department_admin in a participatory space.
   module DepartmentAdmin
   end
