0d58d2f90482231146b178016410965ba7f2c199
Oliver Valls <oliver.vh@coditramuntana.com>
2019-05-13 15:37:30 +0200

    [DOC] Add documentation, and bump version.

--- app/decorators/decidim/user_decorator.rb
+++ app/decorators/decidim/user_decorator.rb
@@ -18,6 +18,11 @@ Decidim::User.class_eval do
     role?('department_admin')
   end
 end
+
+# The following code is a hack to be able to not overwrite Decidim::User::ROLES, which causes many dependency loading order problems,
+# instead it adds department_admin role to the list.
+# The hack un freezes the ROLES array and then adds the new role at the end of the list.
+# This hack can be removed when https://github.com/decidim/decidim/pull/5133 is accepted.
 require 'fiddle'
 
 class Object
@@ -27,3 +32,5 @@ class Object
 end
 Decidim::User::ROLES.unfreeze
 Decidim::User::ROLES << 'department_admin'
+
+# end of HACK
