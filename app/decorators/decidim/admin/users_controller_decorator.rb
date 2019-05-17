93748f54b28126ae90ae339e548163439049d21e
Oliver Valls <oliver.vh@coditramuntana.com>
2019-05-17 14:45:30 +0200

    [DOC] Explain decorator and remove puts.

--- app/decorators/decidim/admin/users_controller_decorator.rb
+++ app/decorators/decidim/admin/users_controller_decorator.rb
@@ -2,13 +2,13 @@
 
 require_dependency 'decidim/admin/users_controller'
 
+# Sort Admins by role and area
 ::Decidim::Admin::UsersController.class_eval do
   alias_method :original_collection, :collection
 
   def collection
     users= original_collection
     @collection||= users.select('*', 'array_to_string(roles) AS sorted_roles').order('sorted_roles')
-    puts ">>>>>>>@#{@collection.pluck(:roles)}"
     Kaminari.paginate_array(@collection.sort { |u1, u2| "#{u1.active_role}||#{u1.areas.first}" <=> "#{u2.active_role}||#{u2.areas.first}" })
   end
 end
