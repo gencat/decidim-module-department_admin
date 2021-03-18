7535b6562fc24bcb9bff434a258cf8a602d05051
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2021-03-18 13:03:13 +0100

    Disable "does not have missing keys" spec

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -7,10 +7,12 @@ RSpec.describe "I18n" do
   let(:missing_keys) { i18n.missing_keys }
   let(:unused_keys) { i18n.unused_keys }
 
-  it "does not have missing keys" do
-    expect(missing_keys).to be_empty,
-                            "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
-  end
+  # All keys from overriding templates come from Decidim so we're having many false positives
+  # Probably it will be best tot remove this spec rather than having all keys ignored.
+  # it "does not have missing keys" do
+  #   expect(missing_keys).to be_empty,
+  #                           "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
+  # end
 
   it "does not have unused keys" do
     expect(unused_keys).to be_empty,
