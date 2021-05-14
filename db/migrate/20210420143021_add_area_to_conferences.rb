3d95ea895a3060f57c4cc080d2a5958c717a34ef
Laura Jaime <laura.jv@coditramuntana.com>
2021-05-14 10:46:31 +0200

    Fix conference migrations

--- db/migrate/20210420143021_add_area_to_conferences.rb
+++ db/migrate/20210420143021_add_area_to_conferences.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-if defined?(Decidim::Conferences)
-  class AddAreaToConferences < ActiveRecord::Migration[5.2]
-    def change
-      add_reference :decidim_conferences, :decidim_area, index: true
-    end
+class AddAreaToConferences < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :decidim_conferences, :decidim_area, index: true if defined?(Decidim::Conferences)
   end
 end
