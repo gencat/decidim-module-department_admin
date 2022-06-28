d58a7b6319de6422b70ab242d11600d903c58b2b
Laura Jaime <laura.jv@coditramuntana.com>
2022-06-28 12:50:40 +0200

    Webpack configuration

---
+++ config/assets.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+# This file is located at `config/assets.rb` of your module.
+
+base_path = File.expand_path("..", __dir__)
+
+# Register the additonal path for Webpacker in order to make the module's
+# stylesheets available for inclusion.
+Decidim::Webpacker.register_path("#{base_path}/app/packs")
+
+# Register the main application's stylesheet include statement:
+# Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/your_component/your_component")
+
+# Register the admin panel's stylesheet include statement:
+# Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/your_component/your_component_admin", group: :admin)
--- lib/decidim/department_admin/engine.rb
+++ lib/decidim/department_admin/engine.rb
@@ -20,10 +20,6 @@ module Decidim
         # root to: "department_admin#index"
       end
 
-      initializer "decidim_department_admin.assets" do |app|
-        app.config.assets.precompile += %w(decidim_department_admin_manifest.js decidim_department_admin_manifest.css)
-      end
-
       # rubocop: disable Lint/ConstantDefinitionInBlock
       initializer "department_admin.permissions_registry" do
         # **
@@ -83,6 +79,10 @@ module Decidim
         end
       end
       # rubocop: enable Lint/ConstantDefinitionInBlock
+
+      initializer "department_admin.webpacker.assets_path" do
+        Decidim.register_assets_path File.expand_path("app/packs", root)
+      end
 
       # make decorators available to applications that use this Engine
       config.to_prepare do
