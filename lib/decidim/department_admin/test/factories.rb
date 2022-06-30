082f59121d889cba06580ae656dab118b7b9948a
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-06-30 14:33:01 +0200

    Rubocopify

--- lib/decidim/department_admin/test/factories.rb
+++ lib/decidim/department_admin/test/factories.rb
@@ -5,7 +5,7 @@ require "decidim/core/test/factories"
 FactoryBot.define do
   factory :department_admin, parent: :user do
     roles { ["department_admin"] }
-    admin_terms_accepted_at { DateTime.now }
+    admin_terms_accepted_at { Time.zone.now }
     transient do
       area { nil }
     end
