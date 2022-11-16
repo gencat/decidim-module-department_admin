05954a38368c70159553fd4b42b7c03bd7624310
Laura Jaime <laura.jv@coditramuntana.com>
2022-11-16 15:20:29 +0100

    Fix Zeitwerk decorators

--- app/decorators/decidim/conferences/update_conference_decorator.rb
+++ app/decorators/decidim/conferences/update_conference_decorator.rb
@@ -2,20 +2,20 @@
 
 # Intercepts the `call` method and forces the Area of the user if it is a
 # department_admin user.
-if Decidim::DepartmentAdmin.conferences_defined?
-  module Decidim::Conferences::UpdateConferenceDecorator
-    def self.decorate
-      Decidim::Conferences::Admin::UpdateConference.class_eval do
-        alias_method :original_call, :call
+module Decidim::Conferences::UpdateConferenceDecorator
+  def self.decorate
+    return unless Decidim::DepartmentAdmin.conferences_defined?
 
-        def call
-          author = form.current_user
-          form.area_id = author.areas.first.id if author.department_admin?
-          original_call
-        end
+    Decidim::Conferences::Admin::UpdateConference.class_eval do
+      alias_method :original_call, :call
+
+      def call
+        author = form.current_user
+        form.area_id = author.areas.first.id if author.department_admin?
+        original_call
       end
     end
   end
-
-  ::Decidim::Conferences::UpdateConferenceDecorator.decorate
 end
+
+::Decidim::Conferences::UpdateConferenceDecorator.decorate
