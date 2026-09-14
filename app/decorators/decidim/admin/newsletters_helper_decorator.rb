fc07c779fb7af48bd3e91633a6e49035fa988bd7
Laura Jaime <laura.jv@coditramuntana.com>
2026-09-14 12:11:51 +0200

    Remove unused condition

--- app/decorators/decidim/admin/newsletters_helper_decorator.rb
+++ app/decorators/decidim/admin/newsletters_helper_decorator.rb
@@ -21,16 +21,9 @@ module Decidim::Admin::NewslettersHelperDecorator
         @spaces_user_can_admin ||= {}
         Decidim.participatory_space_manifests.each do |manifest|
           organization_participatory_space(manifest.name)&.each do |space|
-            if space.respond_to?(:department)
-              next if space.department.blank?
-              next unless current_user.departments.include?(space.department)
-            elsif space.respond_to?(:decidim_area_id)
-              next if space.decidim_area_id.blank?
-              next unless current_user.areas.any?
-              next unless space.decidim_area_id == current_user.areas.first.id
-            else
-              next
-            end
+            next unless space.respond_to?(:department)
+            next if space.department.blank?
+            next unless current_user.departments.include?(space.department)
 
             @spaces_user_can_admin[manifest.name] ||= []
             space_as_option_for_select_data = space_as_option_for_select(space)
